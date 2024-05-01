b72441673dba96ce3448ee465cf35137463a4eae
Sapdotten <semenova.a.p.04@gmail.com>
2024-05-01 20:15:19 +0400

    First commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// Lab3-Graph.cpp : Defines the entry point for the application.
+//
+
+#include "Lab3-Graph.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
